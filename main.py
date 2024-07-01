3bcc1a1d8bd1c4be8724339705cd8ae450660068
Tobias McVey <tobias.mcvey@gmail.com>
2024-07-01 21:05:52 +0200

    program for å konvertere fra csv til json med norske tegn æøå

---
+++ main.py
@@ -0,0 +1,21 @@
+import csv
+import json
+
+file = 'kommuner.csv'
+json_file = 'kommuner.json'
+
+def read_CSV(file, json_file):
+    csv_rows = []
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        field = reader.fieldnames
+        for row in reader:
+            csv_rows.extend([{field[i]:row[field[i]] for i in range(len(field))}])
+        convert_write_json(csv_rows, json_file)
+
+def convert_write_json(data, json_file):
+    with open(json_file, "w") as f:
+        f.write(json.dumps(data ,ensure_ascii=False, sort_keys=False, indent=4, separators=(',', ': ')))
+
+if __name__ == "__main__":
+    read_CSV(file,json_file)
