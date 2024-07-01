512a8deb76cd197cfadca09ed166a486d73450f6
Tobias McVey <tobias.mcvey@gmail.com>
2024-07-01 21:30:04 +0200

    metoder for å konvertere fra json til csv også

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+"""
+Converts csv to json, and json to csv
+
+Allows special characters in other languages such as æøå in Norwegian
+"""
+
 import csv
 import json
 
-csv_file = 'kommuner.csv'
-json_file = 'kommuner.json'
+CSV_FILE = 'kommuner.csv'
+JSON_FILE = 'kommuner.json'
 
-def read_CSV(file, json_file):
+def read_CSV(csv_file, json_file):
     csv_rows = []
     with open(csv_file) as csvfile:
         reader = csv.DictReader(csvfile)
@@ -17,5 +23,16 @@ def convert_write_json(data, json_file):
     with open(json_file, "w") as f:
         f.write(json.dumps(data ,ensure_ascii=False, sort_keys=False, indent=4, separators=(',', ': ')))
 
+def read_json(json_file):
+    return json.loads(open(json_file).read())
+
+def write_csv(data, csv_file):
+    with open(csv_file, 'w+') as outf:
+        writer = csv.DictWriter(outf, data[0].keys())
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+
 if __name__ == "__main__":
-    read_CSV(csv_file,json_file)
+    read_CSV(csv_file=CSV_FILE,json_file=JSON_FILE)
+    # write_csv(read_json(json_file=JSON_FILE), csv_file=CSV_FILE)
